ef3e15493c9a648b6d33d6167e2d94c82ba3ef44
Charles Ma <cma11@email.unc.edu>
2023-10-03 11:48:03 -0400

    Add web integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: 'build/webIntegration.loader.js',
+    dataUrl: 'build/webIntegration.data',
+    frameworkUrl: 'build/webIntegration.framework.js',
+    codeUrl: 'build/webIntegration.wasm',
+  });
+
   return (
-    <h1> Hello, World!</h1>
-  )
+    <div>
+      <h1> Hello, World!</h1>
+      <Unity unityProvider={unityProvider} />
+    </div>
+  );
 }
 
 export default App;
