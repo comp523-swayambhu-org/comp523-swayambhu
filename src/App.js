91df70ab6cad1133449d4f2e9d0ab4a286bcfac8
jessikka12 <jessicawangx24@gmail.com>
2023-10-04 18:58:58 -0400

    extract tutorial dialog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import { Unity, useUnityContext } from 'react-unity-webgl';
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Tutorial from './Tutorial.js'
 
 function App() {
   const { unityProvider, requestPointerLock } = useUnityContext({
@@ -17,14 +12,10 @@ function App() {
   });
 
   const [onGame, setOnGame] = useState(false);
-  const [open, setOpen] = React.useState(true);
-
-  const openTutorial = () => {
-    setOpen(true);
-  };
+  const [tutOpen, setTutOpen] = useState(true);
 
   const closeTutorial = () => {
-    setOpen(false);
+    setTutOpen(false);
     setOnGame(true);
     requestPointerLock();
   };
@@ -32,32 +23,7 @@ function App() {
   return (
     <div>
       <h1 id='title-bar'> Hello, World!</h1>
-      <Dialog
-        open={open}
-        onClose={closeTutorial}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Tutorial"}
-        </DialogTitle>
-        <DialogContent id="alert-dialog-description">
-          <Typography gutterBottom>
-            Use WASD to move around.
-          </Typography>
-          <Typography gutterBottom>
-            Double click ESC to freely move cursor.
-          </Typography>
-          <Typography gutterBottom>
-          Notable parts of the Swayambhu are labelled with annotations. Find the annotations in the menu on the right.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeTutorial} autoFocus>
-            Okay
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <Tutorial open={tutOpen} onClose={closeTutorial}/>
       <Unity 
         unityProvider={unityProvider}
         style={{ height: '100%', width: '100%', visibility: onGame?'visible':'hidden' }}
