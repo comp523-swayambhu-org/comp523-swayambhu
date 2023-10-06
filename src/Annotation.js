c059cf6b3dbcaae51e1bc592408cf1d5a47909dd
jessikka12 <jessicawangx24@gmail.com>
2023-10-06 18:48:07 -0400

    annotation framework

---
+++ src/Annotation.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function Annotation({title, description}) {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            {description}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- src/AnnotationList.js
+++ src/AnnotationList.js
@@ -10,6 +10,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { Typography } from '@mui/material';
+import Annotation from './Annotation.js'
 
 const drawerWidth = 240;
 
@@ -34,29 +35,20 @@ export default function AnnotationList({open}) {
       </Toolbar>
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <Annotation 
+          title="Annotation title" 
+          description={"this is an annotations description"}
+        />
+        <Divider />
+        <Annotation 
+          title="Second Annotation" 
+          description={"maybe we can fill these in by reading from a spreadsheet?"}
+        />
+        <Divider />
+        <Annotation
+          title="Videos?"
+          description={"for videos, we can pass in a boolean to the annotation for if theres a video and display a video if the boolean is true"}
+        />
       </List>
     </Drawer>
   );
