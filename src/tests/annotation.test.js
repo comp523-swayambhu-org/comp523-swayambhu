9e0ad9fa92370e036b297c980dcd4c32dbd999e2
jessikka12 <jessicawangx24@gmail.com>
2023-12-04 09:54:39 -0500

    another assert

--- src/tests/annotation.test.js
+++ src/tests/annotation.test.js
@@ -4,8 +4,14 @@ import Annotation from "../components/Annotation.js";
 //test block
 test("annotation renders", () => {
 // render the component on virtual dom
-render(<Annotation num="1" title="Title" handleShowLocation={console.log('')} />);
+render(<Annotation 
+    num="1" title="Title" 
+    description="test annotation description"
+    handleShowLocation={console.log('')} />);
 
 //assert the expected result
 expect(screen.getByRole('button')).toHaveTextContent("1: Title")
+
+expect(screen.getByText("test annotation description")).toBeInTheDocument()
+
 });
